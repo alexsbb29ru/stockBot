dad31bd84327b958cec512b676887483c9450f45
alexsbb29ru <49026819+alexsbb29ru@users.noreply.github.com>
2020-03-15 20:15:34 +0300

    Add Dima's dll

--- Services/Impl/ExchangeService.cs
+++ Services/Impl/ExchangeService.cs
@@ -1,5 +1,6 @@
 ﻿using BaseTypes;
 using Models;
+using SecuritiesEvaluation;
 using Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -16,19 +17,20 @@ namespace Services.Impl
         {
             var tikersArr = tikers.ToLower().Trim().Split(' ').Distinct();
             
-            List<EvaluationCriteriaModel> evaluationList = new List<EvaluationCriteriaModel>();
+            List<EvaluationCriteria> evaluationList = new List<EvaluationCriteria>();
 
             foreach(var item in tikersArr)
             {
                 evaluationList.Add(EvaluateSecurities(item));
             }
 
-            var resultMessage = "Company: Risk - Earnings";
+            var resultMessage = "Company: Risk | Earnings | CV | Period";
 
             foreach(var item in evaluationList)
             {
                 if (!item.Tiker.ToLower().Contains("error"))
-                    resultMessage += $"\n\r{item.Tiker}: {item.Risk.ToString("F2")} - {item.Earnings.ToString("F2")}";
+                    resultMessage += $"\n\r{item.Tiker}: {item.Risk.ToString("F2")} | {item.Earnings.ToString("F2")}" +
+                        $" | {(item.Risk / item.Earnings)} | {DateTime.Now.Year - 5} - {DateTime.Now.Year}";
                 else
                     resultMessage += $"\n\r{item.Tiker.Replace("error", "")} is wrong tiker";
             }
@@ -36,7 +38,7 @@ namespace Services.Impl
             return resultMessage;
         }
 
-        private EvaluationCriteriaModel EvaluateSecurities(string tiker)
+        private EvaluationCriteria EvaluateSecurities(string tiker)
         {
             try
             {
@@ -44,49 +46,23 @@ namespace Services.Impl
                 //string securities = string.Concat(tiker, ".ME");
                 string securities = tiker;
 
-                List<Candle> candleList = new List<Candle>();
-                List<DividendTick> dividendList = new List<DividendTick>();
+                var startYear = DateTime.Now.Year - 5;
+                var startDate = new DateTime(startYear, 1, 1);
 
-                Task<IReadOnlyList<Candle>> history = Yahoo.GetHistoricalAsync(securities, new DateTime(2015, 1, 1), new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day), Period.Monthly);
-                history.Wait();
+                var endYear = DateTime.Now.Year;
+                var endMonth = DateTime.Now.Month == 1 ? 1: DateTime.Now.Month -1 ;
+                var endDay = DateTime.DaysInMonth(endYear, endMonth);
+                var endDate = new DateTime(endYear, endMonth, endDay);
 
-                candleList.AddRange(history.Result);
-
-                Task<IReadOnlyList<DividendTick>> dividends = Yahoo.GetDividendsAsync(securities, new DateTime(2015, 1, 1), new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day));
-                dividends.Wait();
-
-                dividendList.AddRange(dividends.Result);
-
-                List<decimal> earningsList = new List<decimal>();
-
-                for (int year = 2015; year < DateTime.Now.Year; year++)
-                {
-                    //Получить цены начала и конца года.
-                    Candle first = candleList.First(item => item.DateTime.Year == year);
-                    Candle last = candleList.Last(item => item.DateTime.Year == year);
-
-                    //Получить сумму дивиденда за год.
-                    decimal dividend = dividendList.Where(item => item.DateTime.Year == year).Sum(item => item.Dividend);
-
-                    //Вычислить доходность за год.
-                    decimal earnings = (dividend + (last.Close - first.Close)) / first.Close * 100;
-                    earningsList.Add(earnings);
-                }
-
-                decimal avgEarnings = earningsList.Average();
-
-                //Вычислить стандартное отклонение. Чем выше его значение, тем выше риск, связанный с инвестированием в этот актив, и наоборот.
-                decimal sqrDeviation = earningsList.Sum(item => (decimal)Math.Pow(Convert.ToDouble(item - avgEarnings), 2));
-                decimal stndDeviation = (decimal)Math.Sqrt(Convert.ToDouble(sqrDeviation / 4));
-
-                return new EvaluationCriteriaModel(tiker, stndDeviation, avgEarnings);
+                var evalCriteria = EvaluationMethods.MADEvaluateSecurities(securities, startDate, endDate);
+                return evalCriteria;
             }
             catch (Exception e)
             {
                 var message = e.InnerException.Message ?? e.Message;
                 _logger.Error($"Ошибка получения данных по тикеру {tiker}. Метод {nameof(EvaluateSecurities)} \n\r" +
                     $"{message}");
-                return new EvaluationCriteriaModel(tiker + "error", 0, 0);
+                return new EvaluationCriteria(tiker + "error", 0, 0, 0, 0);
             }
         }
     }
