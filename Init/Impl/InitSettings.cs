9d794f4eda47c5289e7f189fef1385c08d50c57e
Alex <gadddina@gmail.com>
2020-05-07 21:12:46 +0300

    Change to release name

--- Init/Impl/InitSettings.cs
+++ Init/Impl/InitSettings.cs
@@ -17,7 +17,11 @@ namespace Init.Impl
 
         public InitSettings()
         {
-            Token = "1013415129:AAHhl4vTbVwjh89BM-xAkVZV6UOxIRvPMNU";
+            //Test 1141495150:AAHBTDop4zGQWZ6S6-6k6Zb5lPulfE5c8QA
+            //Prod 1013415129:AAHhl4vTbVwjh89BM-xAkVZV6UOxIRvPMNU
+            //Release 1245406455:AAGVuJZkdgNVNWdn1YrZ4Yx42Xrdsxubo0E
+            Token = "1245406455:AAGVuJZkdgNVNWdn1YrZ4Yx42Xrdsxubo0E";//Release
+
             _proxyModel = new ProxyModel()
             {
                 Host = "mssg.me.pp.ru",
