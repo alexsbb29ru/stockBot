457cf684b1c5412d6b51f2904d2bc0e4a2e862af
Alex <gadddina@gmail.com>
2020-05-07 21:11:11 +0300

    Revert "Change to release name"
    This reverts commit 5834fdf790b68efa3b2dd3657ec7fcc42a6b6a55.

--- Init/Impl/InitSettings.cs
+++ Init/Impl/InitSettings.cs
@@ -19,8 +19,7 @@ namespace Init.Impl
         {
             //Test 1141495150:AAHBTDop4zGQWZ6S6-6k6Zb5lPulfE5c8QA
             //Prod 1013415129:AAHhl4vTbVwjh89BM-xAkVZV6UOxIRvPMNU
-            //Release 1245406455:AAGVuJZkdgNVNWdn1YrZ4Yx42Xrdsxubo0E
-            Token = "1245406455:AAGVuJZkdgNVNWdn1YrZ4Yx42Xrdsxubo0E";//Release
+            Token = "1141495150:AAHBTDop4zGQWZ6S6-6k6Zb5lPulfE5c8QA"; //Test
             _proxyModel = new ProxyModel()
             {
                 Host = "mssg.me.pp.ru",
